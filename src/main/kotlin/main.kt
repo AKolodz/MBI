f9822bcdbec2a0afdf33059988cc7740ed4e3092
A.T.Kołodziejczyk <a.t.kolodziejczyk@gmail.com>
2018-01-04 23:39:16 +0100

    More data produced. Length and number of rearrangements

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,8 @@
 import file_interactor.FileInteractorImpl
+import file_interactor.GenomeService
 import file_interactor.GenomeServiceImpl
 import genome.GenomeName
+import genome.GenomeProvider
 import genome.GenomeProviderImpl
 import rearrangement.RearrangementStrategyFactory
 import rearrangement.RearrangementType
@@ -12,10 +14,26 @@ fun main(args: Array<String>) {
     val service = GenomeServiceImpl(FileInteractorImpl())
     val provider = GenomeProviderImpl(service)
 
-    provider.provide(GenomeName.ECOLI)
-            .apply {
-                rearrangementsMaker.rearrange(this,10000,100000,RearrangementType.DUPLICATION)
-                        .apply { service.save("ecoli_10000_100000_dup",this) }
-            }
+    addRearrangementsAndSave(provider, rearrangementsMaker, service, "ecoli_30x_4500000i100000_10_inv",
+            30, 10, 100000, RearrangementType.INVERSION)
+}
 
+fun addRearrangementsAndSave(provider: GenomeProvider,
+                             rearrangementsMaker: RearrangementsMaker,
+                             service: GenomeService,
+                             filename: String,
+                             numberOfRearrangements: Int,
+                             rearrangementLength: Int,
+                             step: Int,
+                             type: RearrangementType) {
+
+    provider.provide(GenomeName.ECOLI)
+            .run {
+                var genome = this
+                val length = genome.length
+                for (i in 1 .. numberOfRearrangements) {
+                    genome = rearrangementsMaker.rearrange(genome, length - i * step, rearrangementLength, type)
+                }
+                genome
+            }.apply { service.save(filename, this) }
 }
