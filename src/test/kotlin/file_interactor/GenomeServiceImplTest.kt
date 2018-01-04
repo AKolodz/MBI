5ae2e71f739c2e04515336d61ec449eb1a9a353d
A.T.Kołodziejczyk <a.t.kolodziejczyk@gmail.com>
2018-01-04 01:15:30 +0100

    GenomeServiceImple tests

---
+++ src/test/kotlin/file_interactor/GenomeServiceImplTest.kt
@@ -0,0 +1,65 @@
+package file_interactor
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.mockito.BDDMockito
+
+@DisplayName("Given instance of an GenomeServiceImpl")
+internal class GenomeServiceImplTest{
+
+    private lateinit var fileInteractor : FileInteractor
+    private lateinit var service : GenomeServiceImpl
+
+    @BeforeEach
+    fun setUp(){
+        fileInteractor = BDDMockito.mock(FileInteractor::class.java)
+        service = GenomeServiceImpl(fileInteractor)
+    }
+
+    @Nested
+    @DisplayName("When loading genome")
+    inner class WhenLoading{
+
+        private val filename = "filename"
+        private val genome = "AACCGGTT"
+
+        @BeforeEach
+        fun setUp(){
+            BDDMockito.given(fileInteractor.readFrom(filename))
+                    .willReturn(genome)
+        }
+
+        @Test
+        fun `then FileInteractor is called`(){
+            service.load(filename)
+            BDDMockito.verify(fileInteractor).readFrom(filename)
+        }
+
+        @Test
+        fun `then genome is returned`(){
+            service.load(filename)
+                    .apply {
+                        assertEquals(genome, this)
+                    }
+
+        }
+    }
+
+    @Nested
+    @DisplayName("When saving genome")
+    inner class WhenSaving{
+
+        private val filename = "filename"
+        private val genome = "AACCGGTT"
+
+        @Test
+        fun `then FileInteractor is called`(){
+            service.save(filename,genome)
+            BDDMockito.verify(fileInteractor).writeTo(filename, genome)
+        }
+    }
+
+}
