da07bf6bbe237795c23baa54c73453a854d6adf9
A.T.Kołodziejczyk <a.t.kolodziejczyk@gmail.com>
2018-01-05 00:50:12 +0100

    Clean up in main. Multiple rearrangements function implemented

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,39 +1,20 @@
 import file_interactor.FileInteractorImpl
-import file_interactor.GenomeService
 import file_interactor.GenomeServiceImpl
 import genome.GenomeName
-import genome.GenomeProvider
 import genome.GenomeProviderImpl
-import rearrangement.RearrangementStrategyFactory
-import rearrangement.RearrangementType
-import rearrangement.RearrangementsMaker
 
 fun main(args: Array<String>) {
 
-    val rearrangementsMaker = RearrangementsMaker(RearrangementStrategyFactory())
     val service = GenomeServiceImpl(FileInteractorImpl())
     val provider = GenomeProviderImpl(service)
-
-    addRearrangementsAndSave(provider, rearrangementsMaker, service, "ecoli_30x_4500000i100000_10_inv",
-            30, 10, 100000, RearrangementType.INVERSION)
+    val genome = provider.provide(GenomeName.ECOLI)
+//    MainFunctions.addSingleTypeRearrangementsAndSave(genome, "test", 2, 100000, 1000000, RearrangementType.DELETION)
+    MainFunctions.addMultipleTypeRearrangements(genome, 10, 100000, 100000)
+            .apply {
+                service.save("test", this)
+            }
 }
 
-fun addRearrangementsAndSave(provider: GenomeProvider,
-                             rearrangementsMaker: RearrangementsMaker,
-                             service: GenomeService,
-                             filename: String,
-                             numberOfRearrangements: Int,
-                             rearrangementLength: Int,
-                             step: Int,
-                             type: RearrangementType) {
 
-    provider.provide(GenomeName.ECOLI)
-            .run {
-                var genome = this
-                val length = genome.length
-                for (i in 1 .. numberOfRearrangements) {
-                    genome = rearrangementsMaker.rearrange(genome, length - i * step, rearrangementLength, type)
-                }
-                genome
-            }.apply { service.save(filename, this) }
-}
+
+
